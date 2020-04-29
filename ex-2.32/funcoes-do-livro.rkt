447214fdc5ab05f7b7341d6e2198f8a113053dce
Rafael Portácio <rafaelportacio19@gmail.com>
2020-04-29 17:32:04 -0300

    ex-2.32 subsets robusto

---
+++ ex-2.32/funcoes-do-livro.rkt
@@ -0,0 +1,29 @@
+#lang racket
+
+;;ctrl C ctrl V de funções do livro que foram úteis pro programa (leve alteraçâo no filter)
+(define (filter predicate sequence)
+  (cond ((null? sequence) null)
+        ((predicate sequence) ;diferente do filter tradicional, o predicado recebe toda a sequence
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+
+(define (accumulate op initial sequence) 
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (flatmap proc seq)
+  (accumulate append null (map proc seq)))
+
+(define (permutations s)
+  (if (null? s)                    ; empty set?
+      (list null)                   ; sequence containing empty set
+      (flatmap (lambda (x)
+                 (map (lambda (p) (cons x p))
+                      (permutations (remove x s))))
+               s)))
+
+(provide filter permutations)
